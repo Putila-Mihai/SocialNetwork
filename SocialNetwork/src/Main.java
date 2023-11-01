4b50dedf5393711ea1db807155177b9257cec32b
MeHigh <128553152+Putila-Mihai@users.noreply.github.com>
2023-11-01 16:38:33 +0200

    Update Main.java

--- SocialNetwork/src/Main.java
+++ SocialNetwork/src/Main.java
@@ -14,15 +14,6 @@ public class Main {
         UserService userService = new UserService(repo);
         UI ui = new UI(userService);
 
-        userService.addUser(new User("Mihai", "Nemteam"));
-        userService.addUser(new User("Alin", "BaiatFin"));
-        userService.addUser(new User("Stefan", "Brailean"));
-        userService.addUser(new User("Decebal", "Aluion"));
-        userService.addUser(new User("Andreeea", "Eleena"));
-        userService.addUser(new User("Ana", "Amaria"));
-        userService.addUser(new User("Ana", "MariaIoana"));
-
-
         ui.run();
     }
 
